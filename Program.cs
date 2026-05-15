fbd2e0fca0db2efd704d35debb9eba1ce016ee71
Lucas roque novaes <lucasroque0802@gmail.com>
2026-05-15 19:20:39 -0300

    feito da quele jeitão

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿int Idade;
+
+Console.Write("quantos anos você tem: ");
+Idade = Convert.ToInt32(Console.ReadLine());
+
+if (Idade >= 18 && Idade <= 67)
+{
+    Console.WriteLine("você pode doar sangue, caso tenha tatuagem você não poderá.");
+}
+
+else
+
+{
+    Console.WriteLine("Você não poderá doar sangue.");
+}
+Console.WriteLine("agradecemos sua vinda!");
